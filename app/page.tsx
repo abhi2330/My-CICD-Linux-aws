dd166a3a447f1fc28e7dc3f4e8cb930fb021f386
abhi2330 <ap2750014@gmail.com>
2025-09-09 14:13:21 +0530

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            shivam Pal {" "}
+            Shivam Pal {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/shiv.tsx
+              app/shivam Pal.tsx
             </code>
             .
           </li>
